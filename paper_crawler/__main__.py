c14a08813c9dc1abbd381522a2353ab69580b8b6
Inhyeok Taylor Baek <bih011122@gmail.com>
2025-03-02 10:10:03 +0900

    main

---
+++ paper_crawler/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Main entry point for running paper_crawler as a module.
+This file allows running the package with 'python -m paper_crawler'.
+"""
+
+from .cli import main
+
+if __name__ == "__main__":
+    main()
